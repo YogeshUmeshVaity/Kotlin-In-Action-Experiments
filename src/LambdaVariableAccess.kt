f4aa8f2204bf712e248fd02ea68df1f4a157a800
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2019-01-31 20:10:21 +0530

    Lambdas: access local variables of a function

--- src/LambdaVariableAccess.kt
+++ src/LambdaVariableAccess.kt
@@ -4,7 +4,22 @@ fun printMessageWithError(messages: List<String>, prefix: String) {
     messages.forEach {println("$prefix $it")} // Can access parameters and local vars inside lambda
 }
 
+fun countErrorClientServerErrors(messages: List<String>) {
+    var clientErrors = 0
+    var serverErrors = 0
+    messages.forEach {
+        if (it.startsWith("4")) {
+            clientErrors++
+        } else if(it.startsWith("5")) {
+            serverErrors++
+        }
+    }
+    println("$clientErrors client error(s) and $serverErrors server error(s)")
+}
+
 fun main() {
-    val messages = listOf<String>("404 Page not found", "403 Network disconnected")
+    val messages = listOf<String>("404 Page not found", "503 Network disconnected", "200 Extension Error")
     printMessageWithError(messages, "Error:")
+
+    countErrorClientServerErrors(messages)
 }
