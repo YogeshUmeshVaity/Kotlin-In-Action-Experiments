ab51175315a7b2c928dd9771f0f35e3f0ed44c6f
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2019-02-04 19:26:20 +0530

    count(), find()

--- src/AllAnyCountFind.kt
+++ src/AllAnyCountFind.kt
@@ -1,19 +1,21 @@
 import com.functional.apis.mapfilter.Person
 
 fun main() {
-    val persons = listOf(
+    val people = listOf(
         Person("Sandy", 24),
         Person("Monty", 30),
         Person("Bunty", 34),
-        Person("Happy", 34))
+        Person("Happy", 31))
 
     /* Define predicate separately.
      * Note: here since, the compiler doesn't know what type of object it's dealing with,
      * you need to specify the object type explicitly. Can't use it.age
      */
     val canBeInClub30 = { person: Person -> person.age <= 30}
-    println("Can all persons be in club of 30 : ${persons.all(canBeInClub30)}")
-    println("Can any person be in club of 30 : ${persons.any(canBeInClub30)}")
+    println("Can all persons be in club of 30?: ${people.all(canBeInClub30)}")
+    println("Can any person be in club of 30? : ${people.any(canBeInClub30)}")
+    println("How many persons can be in club 30? : ${people.count(canBeInClub30)}")
+    println("Find first person that can be in club 30?: ${people.find(canBeInClub30)}")
 
     val numbers = listOf(1, 2, 3)
     /* Both mean the same thing. For readability choose a function that doesn't require
