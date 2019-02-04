9487215b1ffa1f45788e3d3f6f78fc79c4db03c2
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2019-02-04 11:43:18 +0530

    Learn not to repeat a calculation inside a lambda

--- src/FilterMap.kt
+++ src/FilterMap.kt
@@ -5,11 +5,24 @@ data class Person(val name: String, val age: Int)
 fun main() {
     val numbers = listOf(1, 2, 3, 4, 5)
     val evens = numbers.filter { it % 2 == 0 }
+
     println("Original numbers: $numbers")       // Original collection is still intact
     println("Even numbers: $evens")
     println("Squares of evens: ${numbers.filter { it % 2 == 0 }.map { it * it }}")
 
-    val persons = listOf(Person("Sandy", 24), Person("Monty", 30), Person("Bunty", 34))
-    println("Only names of persons: ${persons.map { it.name }}")    // Can use Person::name
-    println("Only names of persons older than 30: ${persons.filter { it.age > 30 }.map { it.name }}")
+    val persons = listOf(
+        Person("Sandy", 24),
+        Person("Monty", 30),
+        Person("Bunty", 34),
+        Person("Happy", 34))
+
+    println("Only names of persons: " +
+            "${persons.map { it.name }}")    // Can use Person::name
+    println("Only names of persons older than 30: " +
+            "${persons.filter { it.age > 30 }.map { it.name }}")
+    println("People with maximum age: " +
+            "${persons.filter { it.age == persons.maxBy(Person::age)?.age}}")
+
+    val maxAge = persons.maxBy(Person::age)?.age // Don't repeat a calculation
+    persons.filter { it.age == maxAge }
 }
