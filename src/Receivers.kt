17b039bf39779c40ea2ea749e19ca8a3e2bdd2a5
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2019-02-15 02:24:28 +0530

    Lambdas with receivers: apply()

--- src/Receivers.kt
+++ src/Receivers.kt
@@ -2,15 +2,39 @@ import java.lang.StringBuilder
 
 fun alphabet(): String {
     val stringBuilder = StringBuilder()
-    with(stringBuilder) {
+    // Here stringBuilder is a receiver object. The receiver object becomes
+    // an implicit 'this' inside the body of the function literal
+    // so that you can access the members of that receiver object without any additional qualifiers,
+    // or access the receiver object using a 'this' expression.
+    return with(stringBuilder) {
         for (letter in 'A'..'Z') {
-            append(letter)
+            this.append(letter)
         }
         append("\nNow I know alphabets")
-        return toString()
+        this.toString()
     }
 }
 
+// The above function can me made more concise using the expression
+// body syntax like following
+fun alphabetConcise() = with(StringBuilder()) {
+    for (letter in 'A'..'Z') {
+        append(letter)
+    }
+    append("\nNow I know the alphabet using with()")
+    toString()
+}
+
+// apply() works just like with() but it always returns the receiver object
+fun alphabetUsingApply() = StringBuilder().apply {
+    for (letter in 'A'..'Z') {
+        append(letter)
+    }
+    append("\nNow I know the alphabet using apply()")
+}.toString()
+
 fun main() {
     println(alphabet())
+    println(alphabetConcise())
+    println(alphabetUsingApply())
 }
