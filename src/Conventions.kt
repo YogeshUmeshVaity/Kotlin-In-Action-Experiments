7860da34f469c4f681122ebf81eae2681e066c44
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2019-02-28 00:53:25 +0530

    Operator overloading: conventions: differant operands and different return types

--- src/Conventions.kt
+++ src/Conventions.kt
@@ -1,11 +1,30 @@
 data class Point(val x: Int, val y: Int) {
+    // Under the hood, the plus() function is called as:     a + b => a.plus(b)
     operator fun plus(other: Point): Point {
         return Point(x + other.x, y + other.y)
     }
 }
 
+// You can define the operator as an extension function as well, the result is the same.
+// It's a common pattern to use convention extension functions for external library classes.
+operator fun Point.times(scale: Double): Point {
+    return Point((x * scale).toInt(), (y * scale).toInt())
+}
+
+// The return type can also be different from the two operand types
+operator fun Char.times(count: Int): String {
+    return this.toString().repeat(count)
+}
+
 fun main() {
     val point1 = Point(10, 20)
     val point2 = Point(30, 40)
+    // You can sum up your objects using the + sign
     println(point1 + point2)
+
+    val point = Point(15, 40)
+    // The operand objects don't have to be of same type.
+    println(point * 1.5)
+
+    println('a' * 3)
 }
