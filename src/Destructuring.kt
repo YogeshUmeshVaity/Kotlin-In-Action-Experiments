fea6749f48d212d3ac86733003a5e7df02b4b3fd
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2019-03-07 21:03:46 +0530

    Return multiple values from a function using Destructuring Declaration Syntax

--- src/Destructuring.kt
+++ src/Destructuring.kt
@@ -1,4 +1,15 @@
+data class NameComponents(val fileName: String, val extension: String)
+
+fun splitFileName(fullName: String): NameComponents {
+    // Collections and Arrays support destructuring up to first five elements
+    val (fileName, extension) = fullName.split(".", limit = 2)
+    return NameComponents(fileName, extension)
+}
+
 fun main() {
+    val (fileName, extension) = splitFileName("String.kt")
+    println("File name: $fileName, Extension: $extension")
+
     val student = Student("Sandy", 30)
     val (name, rollNumber) = student
     println("Name: $name, RollNumber: $rollNumber")
