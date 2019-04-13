22d5593c57a19499086bd9457f0a9e467459285e
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2019-04-13 10:56:03 +0530

    Experiment private var in the constructor

---
+++ src/PrivateVarInConstructor.kt
@@ -0,0 +1,11 @@
+class Musician(private var name: String)
+
+fun main() {
+    val musicianDrv = Musician("Drv")
+    /**
+     * Cannot get or set the property value if it is private in constructor.
+     * It can be set only during construction.
+     */
+    // musicianDrv.name = "Drvn"
+    // println("Musician: ${musicianDrv.name}")
+}
