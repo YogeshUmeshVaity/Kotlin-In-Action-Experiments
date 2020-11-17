59aae5ddc0dc9454c9c33fa6970eebbeca6ef1b3
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-11-17 15:29:53 +0530

    Learn type projections in Kotlin.

--- src/CovariantContravariantDetailExample.kt
+++ src/CovariantContravariantDetailExample.kt
@@ -1,15 +1,17 @@
+
+
 open class Animal {
     fun feed() { println("Animal is being fed") }
 }
 
 /**
- * With the 'out' keyword before type parameter, Herd class is covariant.
- * So, the Herd<Cat> is a subtype of Herd<Animal> and the you can pass Herd<Cat>
- * to the feedAll function which takes Herd<Animal> as its parameter.
+ * With the 'out' keyword before type parameter, typeprojection.Herd class is covariant.
+ * So, the typeprojection.Herd<Cat> is a subtype of typeprojection.Herd<Animal> and the you can pass typeprojection.Herd<Cat>
+ * to the feedAll function which takes typeprojection.Herd<Animal> as its parameter.
  *
- * If you remove the 'out' keyword, the Herd class becomes invariant.
- * So, the Herd<Cat> is not a subtype of Herd<Animal> anymore.
- * And if you try to pass Herd<Cat> to feedAll you get type-mismatch error.
+ * If you remove the 'out' keyword, the typeprojection.Herd class becomes invariant.
+ * So, the typeprojection.Herd<Cat> is not a subtype of typeprojection.Herd<Animal> anymore.
+ * And if you try to pass typeprojection.Herd<Cat> to feedAll you get type-mismatch error.
  */
 class Herd<out T : Animal> {
     private val animalList = listOf<T>()
---
+++ src/typeprojection/TypeProjection.kt
@@ -0,0 +1,31 @@
+package typeprojection
+
+class Crate<T>(private val elements: MutableList<T>) {
+    fun add(t: T) = elements.add(t)     // Consumer allowed
+    fun last(): T = elements.last()     // Producer not allowed: Error
+}
+
+open class Fruit
+
+open class CitrusFruit : Fruit()
+
+class Orange : CitrusFruit()
+
+fun main() {
+    // Only Fruit allowed(no subtyping) because it's invariant
+    val invariantCrate: Crate<Fruit> = Crate<Fruit>(mutableListOf(Fruit(), Orange()))
+    invariantCrate.add(Orange())         // Consumer allowed
+    invariantCrate.last()                // Producer allowed
+
+    // Subtyping allowed because it's covariant
+    val covariantCrate: Crate<out Fruit> = Crate<CitrusFruit>(mutableListOf(Orange()))
+//  covariantCrate.add(Fruit())         // Consumer not allowed: Error
+    covariantCrate.last()               // Producer allowed
+
+    // Subtyping allowed because it's contravariant
+    val contravariantCrate: Crate<in Orange> = Crate<CitrusFruit>(mutableListOf(Orange(), CitrusFruit()))
+    contravariantCrate.add(Orange())    // Consumer allowed
+    contravariantCrate.last()           // Producer allowed: No Error?
+
+    /* Explanation at: https://stackoverflow.com/questions/64835413/type-projections-in-kotlin-different-behaviour-for-use-site-and-declaration-sit */
+}
