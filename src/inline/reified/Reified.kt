941f4a0d0c210bacaac0b766890e1896b215606d
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-03-26 05:13:22 +0530

    Explain reified type parameters with simple examples.

--- src/inline/reified/Reified.kt
+++ src/inline/reified/Reified.kt
@@ -1,27 +1,29 @@
 package inline.reified
 
+import kotlin.reflect.KClass
+
 /**
  * While using generics in Kotlin, we can work with the object(value) of type T.
  */
-fun <T> doSomething(someValue: T) {
-    println("Doing something with value: $someValue")               // OK
+fun <T> doSomething(value: T) {
+    println("Doing something with value: $value")               // OK
 }
 
 /**
  * But we can't work with the type directly. This is because the type argument that we pass to the
  * function is erased at runtime. So, we cannot possibly know exactly which type we are dealing with.
  */
-fun <T> doSomething1(someValue: T) {
+fun <T> doSomething1(value: T) {
     // Error: Cannot use 'T' as reified type parameter. Use a class instead.
 //  println("Doing something with type: ${T::class.simpleName}")     // Error
 }
 
 /**
  * The work around for this in Java was to pass an additional argument specifying the type.
- * This solution is verbose and not very elegant.
+ * This way our code is not affected by type erasure. But this solution is verbose and not very elegant.
  */
-fun <T> doSomething3(someValue: T, type: Class<T>) {
-    println("Doing something with type: ${type::class.simpleName}")  // OK
+fun <T: Any> doSomething4(value: T, type: KClass<T>) {
+    println("Doing something with type: ${type.simpleName}")  // OK
 }
 
 /**
@@ -29,10 +31,11 @@ fun <T> doSomething3(someValue: T, type: Class<T>) {
  * This becomes possible due to the fact inline functions copy the code at call-site. The type
  * parameter that we specify is also copied.
  */
-inline fun <reified T> doSomething3(someValue: T) {
+inline fun <reified T> doSomething3(value: T) {
     println("Doing something with type: ${T::class.simpleName}")    // OK
 }
 
 fun main() {
-    doSomething("Some String")
+    println("Doing something with type: ${String::class.simpleName}")
+
 }
